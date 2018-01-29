784b11e1000177a2a1219ea54921ed5fcaae758e
Vishal <vishal.koosuri1@marist.edu>
2018-01-29 08:26:26 +0000

    Created a new Dialog component

---
+++ src/app/my-dialog/my-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-my-dialog',
+  templateUrl: './my-dialog.component.html',
+  styleUrls: ['./my-dialog.component.css']
+})
+export class MyDialogComponent implements OnInit {
+
+  constructor(
+    private matDialogRef: MatDialogRef<MyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+  }
+  
+  public cancel() {
+    this.matDialogRef.close();
+  }
+
+}
